94b6b0b2977a5f7f803e1e2141e9d9f106a4c6e6
pR916 <20me0114@vvce.ac.in>
2021-03-28 10:24:40 +0000

    Yes

--- Program-8.c
+++ Program-8.c
@@ -20,7 +20,7 @@ void main()
   pt = arr1; // pt store the base address of array arr1
   for (i = 0; i < n; i++) {
       // write the code to calculate sum
-     
+     sum=sum+(pt[i]);
    }
   printf("Sum=%d",sum);
 }
